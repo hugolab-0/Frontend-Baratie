033634b7c0bba3c97b9e9bdad32bce3925929479
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-06-17 11:28:57 -0300

    entendendo a logica para o retorno de 6 produtos

--- src/js/script.js
+++ src/js/script.js
@@ -7,70 +7,97 @@ let indiceAtual = 0;
 const quantidadePorClique = 6;
 
 async function carregarDados() {
-    try {
-        const response = await fetch(`${Url}/v1/baratie/alimento`);
-        const data = await response.json();
-
-        alimentos = data.response.alimento;
-    } catch (error) {
-        console.error('Erro ao carregar alimentos:', error);
-    }
-}
-
-async function ListarMarmitas() {
     const response = await fetch(`${Url}/v1/baratie/alimento`);
     const data = await response.json();
 
+    console.log(data);
+
+    alimentos = data.response.alimento;
+}
+
+async function ListarMarmitas() {
+
+    if (alimentos.length === 0) {
+        await carregarDados();
+    }
+
     const pratosGrid = document.getElementById('pratosGrid');
 
-    data.response.alimento.forEach(prato => {
+    const proximosAlimentos = alimentos.slice(
+        indiceAtual,
+        indiceAtual + quantidadePorClique
+    );
+
+    proximosAlimentos.forEach(prato => {
 
         const calorias =
-        (Number(prato.carboidratos_g) * 4) +
-        (Number(prato.proteinas_g) * 4) +
-        (Number(prato.lipidios_g) * 9);
+            Number(prato.carboidratos_g) * 4 +
+            Number(prato.proteinas_g) * 4 +
+            Number(prato.lipidios_g) * 9;
 
         const card = document.createElement('div');
         card.classList.add('prato-card', 'prato-card--destaque');
 
         card.innerHTML = `
-            <img class="prato-card-img" src="${prato.imagem}" alt="${prato.nome}" />
+            <img
+                class="prato-card-img"
+                src="src/assets/images/marmita-1.svg"
+                alt="${prato.nome}"
+            >
 
-                  <span class="prato-card-kcal"> ${Math.round(calorias)} Kcal </span>
+            <span class="prato-card-kcal">
+                ${Math.round(calorias)} Kcal
+            </span>
 
-                <h3 class="prato-card-title">${prato.nome}</h3>
+            <h3 class="prato-card-title">
+                ${prato.nome}
+            </h3>
 
-    <p class="prato-card-desc">${prato.descricao}</p>
+            <p class="prato-card-desc">
+                ${prato.descricao ?? ''}
+            </p>
 
-    <div class="prato-card-macros">
+            <div class="prato-card-macros">
 
-        <div class="prato-card-macro-item">
-            <span class="prato-card-macro-valor">${prato.proteinas_g}g</span>
-            <span class="prato-card-macro-label">prot</span>
-        </div>
+                <div class="prato-card-macro-item">
+                    <span class="prato-card-macro-valor">
+                        ${prato.proteinas_g}g
+                    </span>
+                    <span class="prato-card-macro-label">
+                        prot
+                    </span>
+                </div>
 
-        <div class="prato-card-macro-item">
-            <span class="prato-card-macro-valor">${prato.carboidratos_g}g</span>
-            <span class="prato-card-macro-label">carb</span>
-        </div>
+                <div class="prato-card-macro-item">
+                    <span class="prato-card-macro-valor">
+                        ${prato.carboidratos_g}g
+                    </span>
+                    <span class="prato-card-macro-label">
+                        carb
+                    </span>
+                </div>
 
-        <div class="prato-card-macro-item">
-            <span class="prato-card-macro-valor">${prato.lipidios_g}g</span>
-            <span class="prato-card-macro-label">gord</span>
-        </div>
+                <div class="prato-card-macro-item">
+                    <span class="prato-card-macro-valor">
+                        ${prato.lipidios_g}g
+                    </span>
+                    <span class="prato-card-macro-label">
+                        gord
+                    </span>
+                </div>
 
-    </div>
-`;
+            </div>
+        `;
 
         pratosGrid.appendChild(card);
     });
+
+    indiceAtual += quantidadePorClique;
+
+  
 }
 
-const btnCarregar = document.getElementById('btnCarregarMais')
-
-btnCarregar.addEventListener('click', async (e) => {
+document.getElementById('btnCarregarMais').addEventListener('click', async (e) => {
     e.preventDefault();
     await ListarMarmitas();
-
-    btnCarregar.style.display = 'none';
 });
